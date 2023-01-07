5fa1f0ec67b6658e074f1e5d18fd3ce8591b39c8
Erali Abdinazarov <91982815+eraliabd@users.noreply.github.com>
2023-01-07 23:24:11 +0500

    add example

--- post_example.py
+++ post_example.py
@@ -1778,4 +1778,26 @@ skill = dictionary.get("skills")[0]
 print(skill)
 
 
-#######  ######
+####### Paskal uchburchagi ######
+# Paskal uchburchagi
+n = int(input("n sonini kiriting: "))
+print("Paskal uchburchagi:")
+
+for i in range(1, n+1):
+    for j in range(0, n-i+1):
+        print(' ', end='')
+
+    c = 1
+    for j in range(1, i+1):
+        print(' ', c, sep='', end='')
+        c = c * (i - j) // j
+    print()
+
+    
+#######  IP Address  #######    
+# # IP Address
+# import ipaddress
+# net = ipaddress.ip_network('74.125.227.0/29')
+
+# for  address in net:
+#     print(address)    
