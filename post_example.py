201b32d6fbb0509abdc1f52471edefe7b5f2b7df
Erali Abdinazarov <91982815+eraliabd@users.noreply.github.com>
2023-02-06 20:17:25 +0500

    add roman number to int number

--- post_example.py
+++ post_example.py
@@ -2183,5 +2183,23 @@ with open("stop_words(uz).txt", "rb") as f:
         
  ###########
 
+# Solution
+def roman_to_int(s):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    result = 0
+
+    for i in range(len(s)):
+        if i > 0 and roman_dict[s[i]] > roman_dict[s[i - 1]]:
+            print("1: ", roman_dict[s[i]] - 2 * roman_dict[s[i - 1]])
+            result += roman_dict[s[i]] - 2 * roman_dict[s[i - 1]]
+        else:
+            print("2: ", roman_dict[s[i]])
+            result += roman_dict[s[i]]
+    return result
+
+input_roman = input("Enter Roman a number: ")
+print(roman_to_int(input_roman))
+
 
 
