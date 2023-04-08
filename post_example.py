83bf9ace69ab13e0d986ba35fe20e3694e73ba73
Erali Abdinazarov <91982815+eraliabd@users.noreply.github.com>
2023-04-08 22:11:43 +0500

    plyer module

--- post_example.py
+++ post_example.py
@@ -2510,3 +2510,18 @@ def main(command):
 home()
 
 ###################
+# Desktop Notifier
+import time
+from plyer import notification
+
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title = "ALERT!",
+            message = "Take a break! It has been 20 seconds!",
+            timeout = 5
+        )
+        time.sleep(20)
+        
+        
+##########
